e168c5c63122033171725a670bc8b1d05959c64d
Bill Langley <brainstormwilly@gmail.com>
2017-04-16 22:58:17 -0700

    Fixed PostGres-SQL conflict converting datetime column

--- db/migrate/20170211073003_change_confirmed_type_in_tasting_tasters.rb
+++ db/migrate/20170211073003_change_confirmed_type_in_tasting_tasters.rb
@@ -1,5 +1,5 @@
 class ChangeConfirmedTypeInTastingTasters < ActiveRecord::Migration[5.0]
   def change
-    change_column :tasting_tasters, :confirmed, :datetime
+    change_column :tasting_tasters, :confirmed, "datetime USING confirmed::timestamp without time zone"
   end
 end
