cb708b858da149be294b9e2c06d2ca74c77ccfac
Bill Langley <brainstormwilly@gmail.com>
2017-04-16 23:06:04 -0700

    Made migration change on datetime to suit PostGres requirements

--- db/migrate/20170211073003_change_confirmed_type_in_tasting_tasters.rb
+++ db/migrate/20170211073003_change_confirmed_type_in_tasting_tasters.rb
@@ -1,5 +1,6 @@
 class ChangeConfirmedTypeInTastingTasters < ActiveRecord::Migration[5.0]
   def change
-    change_column :tasting_tasters, :confirmed, "datetime USING confirmed::timestamp without time zone"
+    remove_column :tasting_tasters, :confirmed
+    add_column :tasting_tasters, :confirmed, :datetime
   end
 end
