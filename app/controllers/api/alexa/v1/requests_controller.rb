be9d14f9659f46d9c959c2e86e3768d81f778881
Bill Langley <brainstormwilly@gmail.com>
2017-05-14 16:48:47 -0700

    Recheck verification

--- app/controllers/api/alexa/v1/requests_controller.rb
+++ app/controllers/api/alexa/v1/requests_controller.rb
@@ -18,21 +18,24 @@ class Api::Alexa::V1::RequestsController < ActionController::Base
       request.body.read
     )
 
-    # Verification invalid
-    return make_plaintext_response("Alexa? Is that you? I am unable to verify.") unless verification_success
+    return make_plaintext_response("Request Verified") if verification_success
+    make_plaintext_response("Request Denied")
 
-    host = current_doorkeeper_host
-
-    # User is not a host
-    return make_plaintext_response("I'm sorry. In order to use me with Yno Wine Tastings, you must be a registered host with an open tasting. Go to ynotasting dot com slash alexa to learn more.") unless host
-
-    open_tasting = Tasting.get_open_for_host(host)
-
-    # No open tastings
-    return make_plaintext_response("Hello #{host.taster.handle}, I don't see any open tastings for you. I can only help you with open tastings. Go to ynotasting dot com slash alexa to learn more.") unless open_tasting
-
-    # Launch request
-    return Alexa::Launch.new().respond(params) if params["request"]["type"] == "LaunchRequest"
+    # # Verification invalid
+    # return make_plaintext_response("Alexa? Is that you? I am unable to verify.") unless verification_success
+    #
+    # host = current_doorkeeper_host
+    #
+    # # User is not a host
+    # return make_plaintext_response("I'm sorry. In order to use me with Yno Wine Tastings, you must be a registered host with an open tasting. Go to ynotasting dot com slash alexa to learn more.") unless host
+    #
+    # open_tasting = Tasting.get_open_for_host(host)
+    #
+    # # No open tastings
+    # return make_plaintext_response("Hello #{host.taster.handle}, I don't see any open tastings for you. I can only help you with open tastings. Go to ynotasting dot com slash alexa to learn more.") unless open_tasting
+    #
+    # # Launch request
+    # return Alexa::Launch.new().respond(params) if params["request"]["type"] == "LaunchRequest"
 
     # Intent request
     # if params["request"]["type"] == "IntentRequest"
