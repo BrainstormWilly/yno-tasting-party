cc27f06fb59d418a72a4496e6378ee4cb01742da
Bill Langley <brainstormwilly@gmail.com>
2017-05-14 21:53:39 -0700

    Retest verification

--- app/controllers/api/alexa/v1/requests_controller.rb
+++ app/controllers/api/alexa/v1/requests_controller.rb
@@ -1,7 +1,7 @@
 class Api::Alexa::V1::RequestsController < ActionController::Base
 
-  prepend_before_action :set_access_token_in_params
-  before_action :doorkeeper_authorize!
+  # prepend_before_action :set_access_token_in_params
+  # before_action :doorkeeper_authorize!
 
 
   def default
@@ -20,19 +20,20 @@ class Api::Alexa::V1::RequestsController < ActionController::Base
 
     # Verification invalid
     return make_plaintext_response("Alexa? Is that you? I am unable to verify.") unless verification_success
+    return make_plaintext_response("Verified");
 
-    host = current_doorkeeper_host
-
-    # User is not a host
-    return make_plaintext_response("I'm sorry. In order to use me with Yno Wine Tastings, you must be a registered host with an open tasting. Go to ynotasting dot com slash alexa to learn more.") unless host
-
-    open_tasting = Tasting.get_open_for_host(host)
-
-    # No open tastings
-    return make_plaintext_response("Hello #{host.taster.handle}, I don't see any open tastings for you. I can only help you with open tastings. Go to ynotasting dot com slash alexa to learn more.") unless open_tasting
-
-    # Launch request
-    return make_plaintext_response("Welcome to Yno Wine Tasting. While tasting you can ask me to rate a wine, get an average rating for a wine, or get tasting statistics. Which would you like to do?") if params["request"]["type"] == "LaunchRequest"
+    # host = current_doorkeeper_host
+    #
+    # # User is not a host
+    # return make_plaintext_response("I'm sorry. In order to use me with Yno Wine Tastings, you must be a registered host with an open tasting. Go to ynotasting dot com slash alexa to learn more.") unless host
+    #
+    # open_tasting = Tasting.get_open_for_host(host)
+    #
+    # # No open tastings
+    # return make_plaintext_response("Hello #{host.taster.handle}, I don't see any open tastings for you. I can only help you with open tastings. Go to ynotasting dot com slash alexa to learn more.") unless open_tasting
+    #
+    # # Launch request
+    # return make_plaintext_response("Welcome to Yno Wine Tasting. While tasting you can ask me to rate a wine, get an average rating for a wine, or get tasting statistics. Which would you like to do?") if params["request"]["type"] == "LaunchRequest"
 
 
 
