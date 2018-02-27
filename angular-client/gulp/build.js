57e6346685e8bb34f5a6d9c7f702ffef8c549824
Bill Langley <brainstormwilly@gmail.com>
2018-02-26 19:46:04 -0800

    prepped for deployment

--- angular-client/gulp/build.js
+++ angular-client/gulp/build.js
@@ -89,7 +89,7 @@ gulp.task('other', function () {
 });
 
 gulp.task('clean', function () {
-  return $.del([path.join(conf.paths.dist, '/'), path.join(conf.paths.tmp, '/')]);
+  return $.del([path.join(conf.paths.dist, '/'), path.join(conf.paths.tmp, '/')], {force:true}, done);
 });
 
 gulp.task('build', ['html', 'fonts', 'other']);
