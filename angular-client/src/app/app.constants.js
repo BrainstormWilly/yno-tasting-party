5ee1171341ea9edb1f868a3910dbf4fe99f7fa2c
Bill Langley <brainstormwilly@gmail.com>
2018-03-04 00:25:39 -0800

    localized deploy constants

--- angular-client/src/app/app.constants.js
+++ angular-client/src/app/app.constants.js
@@ -1,8 +1,8 @@
 export const AppConstants = {
 
-  authUrl: "https://www.ynotasting.com",
-  invitationUrl: "https://www.ynotasting.com/auth/invitation",
-  apiUrl: "https://www.ynotasting.com/api/v1",
+  authUrl: "/",
+  invitationUrl: "/auth/invitation",
+  apiUrl: "/api/v1",
   baseUrl: "/",
   enableDebug: true
 
