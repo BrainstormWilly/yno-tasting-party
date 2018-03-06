d12450d68fb23160bf90609f60e9fb90dca92f0c
Bill Langley <brainstormwilly@gmail.com>
2018-03-05 22:25:21 -0800

    remove mp min file

--- angular-client/src/app/app.component.js
+++ angular-client/src/app/app.component.js
@@ -6,8 +6,6 @@ export const AppComponent = {
     constructor($location, $window, $log){
       'ngInject';
 
-      $log.log(moment());
-
       if( $location.host()=="www.ynotasting.com" ) $window.location.href = "https://ynotasting.com" + $location.path();
       if( $location.host()=="ynotasting.com" && $location.protocol()=="http" ) $window.location.href = "https://ynotasting.com" + $location.path();
 
