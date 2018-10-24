063445b00f609dbd2ba8ded02c280286496bd91b
Bill Langley <brainstormwilly@gmail.com>
2018-10-23 18:11:02 -0700

    ready new design for deploy

--- angular-client/src/app/app.constants.js
+++ angular-client/src/app/app.constants.js
@@ -1,11 +1,11 @@
 export const AppConstants = {
 
   // PRODUCTION
-  // authUrl: "",
-  // invitationUrl: "/auth/invitation",
-  // apiUrl: "/api/v1",
-  // baseUrl: "/",
-  // enableDebug: true
+  authUrl: "",
+  invitationUrl: "/auth/invitation",
+  apiUrl: "/api/v1",
+  baseUrl: "/",
+  enableDebug: true
 
   // MOBILE TESTING
   // authUrl: "http://10.0.0.53:8000",
@@ -15,9 +15,9 @@ export const AppConstants = {
   // enableDebug: true
 
   // DESKTOP TESTING
-  authUrl: "http://localhost:8000",
-  invitationUrl: "http://localhost:8000/auth/invitation",
-  apiUrl: "http://localhost:8000/api/v1",
-  baseUrl: "/",
-  enableDebug: true 
+  // authUrl: "http://localhost:8000",
+  // invitationUrl: "http://localhost:8000/auth/invitation",
+  // apiUrl: "http://localhost:8000/api/v1",
+  // baseUrl: "/",
+  // enableDebug: true
 }
