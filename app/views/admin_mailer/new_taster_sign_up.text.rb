78df6b212d164910c59ecee33b0d55050bb1d9d8
Bill Langley <brainstormwilly@gmail.com>
2018-10-28 21:13:21 -0700

    ready to merge

--- app/views/admin_mailer/new_taster_sign_up.text.rb
+++ app/views/admin_mailer/new_taster_sign_up.text.rb
@@ -1,3 +1,5 @@
 
 Yno Taster: <%= @taster.name %> (<%= @taster.handle %>)
 Email: <%= @taster.user.email %>
+Created: <%= @taster.created_at.in_time_zone("Pacific Time (US & Canada)").strftime("%a, %b %-d %Y %I:%M%P %Z") %>
+Status: <%= @taster.status %>
