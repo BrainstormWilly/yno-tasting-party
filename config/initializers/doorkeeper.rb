2f7a0404478532bc261a3d92eb56f9bbcea47f6d
Bill Langley <brainstormwilly@gmail.com>
2018-11-24 14:30:35 -0800

    Alexa fixes

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -8,8 +8,8 @@ Doorkeeper.configure do
     # Put your resource owner authentication logic here.
     # Example implementation:
     #   User.find_by_id(session[:user_id]) || redirect_to(new_user_session_url)
-    #current_user || warden.authenticate!(scope: :user)
-    current_user || redirect_to("/signin/alexa")
+    current_user || warden.authenticate!(scope: :user)
+    # current_user || redirect_to("/signin/alexa")
   end
 
   # If you want to restrict access to the web interface for adding oauth authorized applications, you need to declare the block below.
